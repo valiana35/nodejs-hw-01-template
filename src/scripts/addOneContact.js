d7c06324e22e43f68e37c705486a163f30aed102
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-05-23 22:33:56 +0300

    node.jshw1

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+        const contact = createFakeContact();
+        await fs.appendFile(PATH_DB, JSON.stringify(contact, null, 2), 'utf-8');
+        console.log(contact);
+};
 
 await addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    let contacts = [];
+    for (let i = 0; i < number; i++) {
+        contacts.push(createFakeContact());
+    }
+    await fs.appendFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+};
 
 await generateContacts(5);
